7730cb744a78f4689f1becbccb0026bc0620d4e3
dudek-igor <dudekigor@gmail.com>
2021-11-16 11:50:56 +0100

    Try to figure it out socket connection

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
 import { connect, set } from 'mongoose';
+import { io, Socket } from 'socket.io-client';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { dbConnection } from '@databases';
@@ -20,19 +21,24 @@ class App {
   public app: express.Application;
   public port: string | number;
   public env: string;
+  public socket_connection_hub: Socket;
 
   constructor(routes: Routes[]) {
     this.app = express();
     this.port = process.env.PORT || 3000;
     this.env = process.env.NODE_ENV || 'development';
-
     this.connectToDatabase();
+    this.connectToHubViaSocket();
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeSwagger();
     this.initializeErrorHandling();
   }
-
+  //@info getter for server instance
+  public getServer() {
+    return this.app;
+  }
+  //@info Mount App Listen
   public listen() {
     this.app.listen(this.port, () => {
       logger.info(`=================================`);
@@ -41,19 +47,56 @@ class App {
       logger.info(`=================================`);
     });
   }
-
-  public getServer() {
-    return this.app;
-  }
-
+  //@info Connect with DB
   private connectToDatabase() {
     if (this.env !== 'production') {
       set('debug', true);
     }
-
     connect(dbConnection.url, dbConnection.options);
   }
+  //@info Connect with Hub via socket
+  private connectToHubViaSocket() {
+    this.socket_connection_hub = io(config.get('socket.mec_uri'), {
+      // transports: ['websocket'],
+      // transports: ['websocket'], config: [.connectParams(["EIO": "3"])],
+      // maxHttpBufferSize: 1e8,
+      // transports: ['websocket'],
+      // transports: ['websocket', 'polling'],
+      // reconnection: true,
+      // rejectUnauthorized: false,
+      // path: '/socket.io/',
+      // upgrade: true,
+      // rememberUpgrade: true,
+      // withCredentials: true,
+      // rejectUnauthorized: true,
+      // withCredentials: true,
+      // port: '80',
+      // extraHeaders: {
+      //   // Connection: 'Upgrade',
+      //   // 'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+      //   // 'Sec-WebSocket-Version': '13',
+      //   Upgrade: 'websocket',
+    });
+    this.socket_connection_hub
+      .on('connect', () => {
+        console.log('Socket connected');
+        // console.log(this.socket_hub.connected); // true
+      })
+      .on('disconnect', () => {
+        console.log('Socket diconnected');
+        // console.log(this.socket_hub.connected); // false
+      })
+      .on('error', () => {
+        console.log('Socket diconnected');
+      })
+      .on('connect_error', error => {
+        console.log('Connection Socket Error');
+        console.log(error);
+        // socket.connect();
+      });
+  }
 
+  //@info Init middlewares before routes
   private initializeMiddlewares() {
     this.app.use(morgan(config.get('log.format'), { stream }));
     this.app.use(cors({ origin: config.get('cors.origin'), credentials: config.get('cors.credentials') }));
@@ -64,29 +107,29 @@ class App {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cookieParser());
   }
-
+  //@info Mount Routes
   private initializeRoutes(routes: Routes[]) {
     routes.forEach(route => {
-      this.app.use('/', route.router);
+      this.app.use('/api/v1', route.router);
     });
   }
-
+  //@info Init Swagger
   private initializeSwagger() {
     const options = {
       swaggerDefinition: {
         info: {
           title: 'REST API',
           version: '1.0.0',
-          description: 'Example docs',
+          description: 'MEC docs',
         },
       },
       apis: ['swagger.yaml'],
     };
 
     const specs = swaggerJSDoc(options);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+    this.app.use('/api/v2/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
   }
-
+  //@info Init Error Handler Middleware
   private initializeErrorHandling() {
     this.app.use(errorMiddleware);
   }

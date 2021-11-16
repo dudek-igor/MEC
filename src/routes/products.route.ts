a73e5dffc80daebd044acc482e88c08600034e76
dudek-igor <dudekigor@gmail.com>
2021-11-16 22:52:28 +0100

    Created products routes

---
+++ src/routes/products.route.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import ProductsController from '@controllers/users.controller';
+import { Routes } from '@interfaces/routes.interface';
+
+class ProductsRoute implements Routes {
+  public path = '/products';
+  public router = Router();
+  public productsController = new ProductsController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.productsController.getAllProducts);
+    this.router.get(`${this.path}/:id`, this.productsController.getProductById);
+  }
+}
+
+export default ProductsRoute;

57e8814ce98f2d820b0618ba27c0465dd9fe8eef
dudek-igor <dudekigor@gmail.com>
2021-11-16 22:52:13 +0100

    Created product model

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,25 +1,37 @@
 import { model, Schema, Document } from 'mongoose';
 import { Product } from '@/interfaces/products.interface';
 
-const productSchema: Schema = new Schema({
-  productId: {
-    type: Number,
-    required: true,
-    unique: true,
+const productSchema: Schema = new Schema(
+  {
+    _id: false,
+    productId: {
+      type: Number,
+      primaryKey: true,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    sold_times: {
+      type: Number,
+      default: 0,
+      // required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
+  {
+    versionKey: false,
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  stock: {
-    type: Number,
-    required: true,
-  },
-});
+);
 
 const articleModel = model<Product & Document>('Product', productSchema);
 

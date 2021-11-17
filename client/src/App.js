6538f73237ae4ba699b7847152d3a94d3271197d
dudek-igor <dudekigor@gmail.com>
2021-11-17 15:24:27 +0100

    Setup React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Hello There </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

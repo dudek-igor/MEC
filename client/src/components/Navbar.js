b4bd1d1afa18e06591dd9d244072d8f2225b41ea
dudek-igor <dudekigor@gmail.com>
2021-11-18 17:19:20 +0100

    Create navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import Button from '@mui/material/Button';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/hot-deals">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <AppBar position="relative" color="transparent">
+      <Toolbar
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Button startIcon={<StorefrontIcon/>} sx={{ mr: 2 }} component={Link} size="small" variant="outlined" to="/">
+          Home
+        </Button>
+        <Button startIcon={<LocalFireDepartmentIcon/>} component={Link} size="small" variant="outlined" to="/hot-deals">
+          Hot Deals
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 

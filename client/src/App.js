d656298ffb425a2a68aaeb72b8deaf3dbac62020
dudek-igor <dudekigor@gmail.com>
2021-11-18 09:41:18 +0100

    Setup app skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, ShopCart } from './components';
+import { Home, HotDeals } from './views';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        // Get User Data
+        const data = await axios.get('/api/v1/');
+        console.log(data);
+        // setLogged(data.success);
+      } catch (error) {
+        // If error => logout user
+        // console.error({ error });
+        // setLogged(false);
+      }
+      // setComponentMount(true);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> Hello There </p>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <ShopCart/>
+      <Routes>
+        <Route exact path="/hot-deals" element={<HotDeals />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 

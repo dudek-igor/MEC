dc06ea918d13af144a4b395dbca1b6d2108071b4
dudek-igor <dudekigor@gmail.com>
2021-11-20 00:23:50 +0100

    Setup socket FE

--- client/src/context/socket.context.js
+++ client/src/context/socket.context.js
@@ -1,83 +1,50 @@
-// import React, { createContext, useReducer, useState, useCallback, useEffect } from 'react';
+import { useEffect, useContext, createContext } from 'react';
+import useWebSocket from 'react-use-websocket';
+import { GlobalContext } from '../context/global.context';
+import { setupSocketConnction, handleSocketMessages } from '../actions/global.actions';
 // import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 // // Create context
-// export const SocketContext = createContext();
+export const SocketContext = createContext();
 
-// //Provider Component
-// export const SocketProvider = ({ children }) => {
-//   const [socketUrl, setSocketUrl] = useState('wss://echo.websocket.org');
-//   const [messageHistory, setMessageHistory] = useState([]);
+export const SocketProvider = ({ children }) => {
+  const { dispatch } = useContext(GlobalContext);
 
-//   const {
-//     sendMessage,
-//     lastMessage,
-//     readyState,
-//   } = useWebSocket();
-
-
-//   const socket = '';
-//   //   const socket = io();
-//   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
-// };
-
-import React, { useState, useCallback, useEffect } from 'react';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
-
-export const SocketProvider = ({children}) => {
-  //Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState('wss://echo.websocket.org');
-  const [messageHistory, setMessageHistory] = useState([]);
-//   'ws://shielded-escarpment-64139.herokuapp.com:8080'
-  const {
-    sendMessage,
-    lastMessage,
-    readyState,
-  } = useWebSocket('ws://localhost:8080');
+  //   'ws://shielded-escarpment-64139.herokuapp.com:8080'
+  const { sendMessage, lastMessage, readyState } = useWebSocket('ws://localhost:8080', {
+    // Shuld i update data on reconnect!
+    shouldReconnect: closeEvent => {
+      return true;
+    },
+  });
+  //@info useEffect for check socket state
+  useEffect(() => {
+    //@info Setup Socket Connection
+    setupSocketConnction(dispatch, readyState);
+  }, [dispatch, readyState]);
 
   useEffect(() => {
-    if (lastMessage !== null) {
-      setMessageHistory(prev => prev.concat(lastMessage));
-    }
-  }, [lastMessage, setMessageHistory]);
+    //@info Handle Socket Messages
+    handleSocketMessages(dispatch, lastMessage);
+  }, [dispatch, lastMessage]);
 
-  const handleClickChangeSocketUrl = useCallback(() =>
-    setSocketUrl('wss://demos.kaazing.com/echo'), []);
-
-  const handleClickSendMessage = useCallback(() =>
-    sendMessage('Hello'), []);
-
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState];
+  // const handleClickSendMessage = useCallback(() => sendMessage('Hello'), [sendMessage]);
 
   return (
     <>
-    {children}
-    <div>
-      <button
-        onClick={handleClickChangeSocketUrl}
-      >
-        Click Me to change Socket Url
-      </button>
-      <button
-        onClick={handleClickSendMessage}
-        disabled={readyState !== ReadyState.OPEN}
-      >
-        Click Me to send 'Hello'
-      </button>
-      <span>The WebSocket is currently {connectionStatus}</span>
-      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-      <ul>
-        {messageHistory
-          .map((message, idx) => <span key={idx}>{message ? message.data : null}</span>)}
-      </ul>
-    </div>
-    
+     <SocketContext.Provider value={sendMessage}>{children}</SocketContext.Provider>
+      {/* <div>
+        <button onClick={handleClickSendMessage} disabled={readyState !== ReadyState.OPEN}>
+          Click Me to send 'Hello'
+        </button>
+        {/* <span>The WebSocket is currently {connectionStatus}</span> */}
+        {/* {lastMessage ? <span>Last message: {lastMessage.data}</span> : null} */}
+        {/* <ul>
+          {messageHistory.map((message, idx) => (
+            <span key={idx}>{message ? message.data : null}</span>
+          ))}
+        </ul> */}
+      {/* </div> */} 
     </>
   );
 };

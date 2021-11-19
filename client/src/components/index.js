251fb1f0be2f2483292fb87cebe5d942e8df44fa
dudek-igor <dudekigor@gmail.com>
2021-11-19 14:55:09 +0100

    Fixed import

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from './Navbar';
-export { default as ShopCart } from './ShopCart';
+export { default as ShopCart } from './ShopCard';
 export { default as ProductCard } from './ProductCard';

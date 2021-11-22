78842ea3907a65afde9aaacaed5e1c70fa8c8542
dudek-igor <dudekigor@gmail.com>
2021-11-22 09:25:52 +0100

    Setup database for heroku

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,5 +1,10 @@
+import config from 'config';
+import { dbConfig } from '@interfaces/db.interface';
+
+const { host, port, database }: dbConfig = config.get('dbConfig');
+
 export const dbConnection = {
-  url: process.env.DATABASE_URI,
+  url: process.env?.DOCKER_COMPOSE ? `mongodb://${host}:${port}/${database}` : process.env.DATABASE_URI,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
